d2c3b0e69f0a957777d0871501ceddbe45de3212
yewyewXD <yewyew6933@gmail.com>
2022-05-19 20:01:53 +0800

    add parallax (unclean)

--- components/Home/HeroSection.jsx
+++ components/Home/HeroSection.jsx
@@ -1,18 +1,96 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const HeroSection = () => {
-  return (
-    <section className="HeroSection h-screen w-full relative flex justify-center items-center">
-      <h2 className="absolute text-blue-900 text-center">
-        Lets decentralize the world
-        <br />
-        <span className="text-4xl">Together</span>
-      </h2>
+  useEffect(() => {
+    const rocks = document.getElementById("rocks");
+    const text = document.getElementById("text");
+    const btn = document.getElementById("btn");
 
-      <button className="px-2 py-7 bg-white text-blue-900 rounded-3xl">
-        Explore
-      </button>
-    </section>
+    function parallaxEffect() {
+      const value = window.scrollY;
+      rocks.style.top = value * -0.12 + "px";
+      text.style.top = 50 + value * -0.1 + "%";
+      btn.style.marginTop = value * 1.4 + "px";
+    }
+
+    window.addEventListener("scroll", parallaxEffect);
+
+    () => window.removeEventListener("scroll", parallaxEffect);
+  }, []);
+
+  return (
+    <>
+      <section>
+        <h2 id="text">
+          <span>It&apos;s time for us to</span>
+          <br />
+          Decentralize
+        </h2>
+        <a href="#sec" id="btn">
+          Explore
+        </a>
+        <img src="/images/rocks.png" id="rocks" />
+        <img src="/images/water.png" id="water" />
+      </section>
+
+      <div className="sec" id="sec">
+        <h2>Parallax Scrolling Effects</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero iste
+          laboriosam iusto sapiente voluptatum a, incidunt culpa quae.
+          Necessitatibus vero exercitationem accusantium nemo culpa ipsa tempora
+          quia? Aliquam, nemo doloribus. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Id quidem dolore atque maiores laudantium, corporis
+          quam quisquam vero repellat ad nobis asperiores incidunt veritatis
+          neque eum delectus consequuntur quibusdam eaque. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Impedit cumque molestiae fugit
+          ratione aliquam suscipit, perspiciatis in quod molestias quisquam
+          doloribus at tempore. Minima aperiam molestias in nobis eaque ipsam.
+          <br />
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero iste
+          laboriosam iusto sapiente voluptatum a, incidunt culpa quae.
+          Necessitatibus vero exercitationem accusantium nemo culpa ipsa tempora
+          quia? Aliquam, nemo doloribus. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Id quidem dolore atque maiores laudantium, corporis
+          quam quisquam vero repellat ad nobis asperiores incidunt veritatis
+          neque eum delectus consequuntur quibusdam eaque.
+          <br />
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero iste
+          laboriosam iusto sapiente voluptatum a, incidunt culpa quae.
+          Necessitatibus vero exercitationem accusantium nemo culpa ipsa tempora
+          quia? Aliquam, nemo doloribus. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Id quidem dolore atque maiores laudantium, corporis
+          quam quisquam vero repellat ad nobis asperiores incidunt veritatis
+          neque eum delectus consequuntur quibusdam eaque. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Impedit cumque molestiae fugit
+          ratione aliquam suscipit, perspiciatis in quod molestias quisquam
+          doloribus at tempore. Minima aperiam molestias in nobis eaque ipsam.
+          <br />
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero iste
+          laboriosam iusto sapiente voluptatum a, incidunt culpa quae.
+          Necessitatibus vero exercitationem accusantium nemo culpa ipsa tempora
+          quia? Aliquam, nemo doloribus. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Id quidem dolore atque maiores laudantium, corporis
+          quam quisquam vero repellat ad nobis asperiores incidunt veritatis
+          neque eum delectus consequuntur quibusdam eaque.
+          <br />
+          <br />
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero iste
+          laboriosam iusto sapiente voluptatum a, incidunt culpa quae.
+          Necessitatibus vero exercitationem accusantium nemo culpa ipsa tempora
+          quia? Aliquam, nemo doloribus. Lorem ipsum, dolor sit amet consectetur
+          adipisicing elit. Id quidem dolore atque maiores laudantium, corporis
+          quam quisquam vero repellat ad nobis asperiores incidunt veritatis
+          neque eum delectus consequuntur quibusdam eaque. Lorem ipsum dolor sit
+          amet consectetur adipisicing elit. Impedit cumque molestiae fugit
+          ratione aliquam suscipit, perspiciatis in quod molestias quisquam
+          doloribus at tempore. Minima aperiam molestias in nobis eaque ipsam.
+        </p>
+      </div>
+    </>
   );
 };
 
