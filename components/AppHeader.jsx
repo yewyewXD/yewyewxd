22f7839fef85aeb2260e6e892d1db3003c76804f
yewyewXD <yewyew6933@gmail.com>
2022-05-28 19:33:08 +0800

    hero layout, section-title style class

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AppHeader = () => {
-  return <header>Appheader</header>;
+  return <header></header>;
 };
 
 export default AppHeader;
--- components/Home/HeroSection.jsx
+++ components/Home/HeroSection.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import Image from "next/image";
+import styles from "../../styles/Home/hero.module.scss";
 
 const HeroSection = () => {
-  return <section className="">test</section>;
+  return (
+    <section className="py-20">
+      <div className="container grid grid-cols-2">
+        <div className="flex flex-col">
+          <big className="section-title">web3 react developer</big>
+          <h1 className="my-5">Together, we decentralize</h1>
+          <p className="text-light">
+            Buy the new super rare piece of digital art and make your own
+            collection more valuable!
+          </p>
+
+          <div className="mt-5 flex">
+            <button>View Projects</button>
+            <button>Resume</button>
+          </div>
+        </div>
+
+        <div className="flex justify-end">
+          <div className={styles.Card}>
+            <Image
+              src="/images/hero-card.png"
+              alt=""
+              layout="fill"
+              quality={100}
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HeroSection;
