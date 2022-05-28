5d46414ad3bc86c24c81aa967e89a7dbe3dd0a59
yewyewXD <yewyew6933@gmail.com>
2022-05-28 18:05:04 +0800

    rm moralis provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
 import "../styles/globals.scss";
-import { MoralisProvider } from "react-moralis";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <MoralisProvider
-      serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER}
-      appId={process.env.NEXT_PUBLIC_MORALIS_APPID}
-    >
-      <Component {...pageProps} />
-    </MoralisProvider>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
