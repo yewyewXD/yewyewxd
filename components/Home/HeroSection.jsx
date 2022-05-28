f6f576741ff76a5d22e4bab4b1a0f38e3daf9980
yewyewXD <yewyew6933@gmail.com>
2022-05-28 20:30:48 +0800

    add link for resume

--- components/Home/HeroSection.jsx
+++ components/Home/HeroSection.jsx
@@ -3,11 +3,11 @@ import React from "react";
 import Image from "next/image";
 import styles from "../../styles/Home/hero.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faDownload, faStar } from "@fortawesome/free-solid-svg-icons";
 
 const HeroSection = () => {
   return (
-    <section className="py-20">
+    <section className="py-32">
       <div className="container grid grid-cols-2">
         <div className="flex flex-col">
           <div className="flex flex-col relative">
@@ -23,7 +23,9 @@ const HeroSection = () => {
               </a>
               <div className="ml-7 flex items-center underline cursor-pointer transition-opacity duration-300 hover:opacity-80">
                 <FontAwesomeIcon icon={faDownload} className="w-3 mr-1" />
-                <big>Resume</big>
+                <a href="/docs/resume.pdf" target="_blank">
+                  <big>Resume</big>
+                </a>
               </div>
             </div>
 
@@ -40,9 +42,31 @@ const HeroSection = () => {
             />
           </div>
 
-          <div className="mt-20">
-            <h2>Top Rated</h2>
-          </div>
+          <a
+            href="https://www.upwork.com/freelancers/~01186da3d6211b3ce4"
+            rel="noopener noreferrer"
+            target="_blank"
+            className="mt-20 flex items-center hover:opacity-80 transition-opacity duration-300"
+          >
+            <div className="flex items-center border-r pr-7">
+              <Image
+                src="/images/general-upwork.png"
+                alt=""
+                className="rounded-full"
+                height={40}
+                width={40}
+              />
+              <big className="text-light ml-3 font-semibold">TOP RATED</big>
+            </div>
+
+            <div className="flex items-center pl-9">
+              <h2>4.9</h2>
+              <div className="text-light ml-4 flex items-center">
+                <FontAwesomeIcon icon={faStar} className="w-3 mr-1" />
+                <small className="font-semibold">AVG RATING</small>
+              </div>
+            </div>
+          </a>
         </div>
 
         <div className="flex justify-end">
