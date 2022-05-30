31b7940907f60669dcc7d11879071e16c5b109cd
yewyewXD <yewyew6933@gmail.com>
2022-05-31 05:39:22 +0800

    setup header

--- components/AppHeader.jsx
+++ components/AppHeader.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import navItems from "../json/navItems.json";
+import Link from "next/link";
 
 const AppHeader = () => {
-  return <header></header>;
+  return (
+    <header className="mt-4">
+      <div className="container flex justify-between items-center">
+        <div className="section-title">yewyewXD</div>
+
+        <nav className="flex items-center">
+          {navItems.map((navItem) => (
+            <div
+              className={navItem.subLinks ? "NavItemExpand relative" : ""}
+              key={navItem.link}
+            >
+              <Link href={navItem.link} passHref>
+                <span className="py-4 px-5 flex items-center cursor-pointer">
+                  {navItem.name}
+                </span>
+              </Link>
+
+              {navItem.subLinks && (
+                <div className="NavDropdown opacity-0 cursor-pointer whitespace-nowrap absolute flex flex-col pointer-events-none z-50">
+                  {navItem.subLinks.map((subLink) => (
+                    <Link href={subLink.link} key={subLink.link} passHref>
+                      <span>{subLink.name}</span>
+                    </Link>
+                  ))}
+                </div>
+              )}
+            </div>
+          ))}
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default AppHeader;
