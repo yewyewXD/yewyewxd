959978f785845f8c45c74359d107f790329252ce
yewyewXD <yewyew6933@gmail.com>
2022-05-31 06:51:39 +0800

    init story section

--- components/About/StorySection.jsx
+++ components/About/StorySection.jsx
@@ -2,9 +2,17 @@ import React from "react";
 
 const StorySection = () => {
   return (
-    <section>
+    <section className="py-24">
       <div className="container text-center">
-        <big className="section-title">OUR STORY</big>
+        <big className="section-title">About me</big>
+        <h2 className="mt-2 mb-4">A little back story</h2>
+        <div className="text-dark mx-auto w-8/12">
+          I appreciate that you made it here. This video will explain how I got
+          into coding, why I stayed, and some crazy journey I have been through.
+          Enjoy!
+        </div>
+
+        <div className="h-96 w-10/12 mx-auto bg-gray-700 mt-12 rounded-3xl"></div>
       </div>
     </section>
   );
