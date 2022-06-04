2d7bb0e434dbbc73c4d379c25366ab5efa68460a
yewyewXD <yewyew6933@gmail.com>
2022-06-04 17:03:48 +0800

    add hover overflow and links to projects

--- components/Home/ProjectSection.jsx
+++ components/Home/ProjectSection.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import Image from "next/image";
 import styles from "../../styles/pages/home.module.scss";
 import projects from "../../json/projects.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLink } from "@fortawesome/free-solid-svg-icons";
 
 const Card = ({ name, description, image, size, style }) => {
   return (
     <div className={styles.ProjectCard} style={style}>
+      <div className="h-full w-full absolute opacity-0 transition-opacity duration-200 bg-black top-0 left-0 hover:opacity-80 z-50 all-center">
+        <a
+          href="https://github.com/yewyewXD"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon
+            icon={faLink}
+            className="w-10 mr-8 bg-main text-black px-1 py-2 rounded-md transition duration-200 hover:scale-110 cursor-pointer"
+          />
+        </a>
+
+        <a
+          href="https://github.com/yewyewXD"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="relative h-10 w-10 transition duration-200 hover:scale-110 cursor-pointer"
+        >
+          <Image src="/images/brand/github.png" alt="" layout="fill" />
+        </a>
+      </div>
       <div
         className={styles[`ProjectCard__Img${size}`]}
         style={{ backgroundImage: `url('/images/project/${image}')` }}
