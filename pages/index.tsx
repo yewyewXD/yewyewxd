b6ea2c2ae0e8341e97592d8610bb7e1aaa9cdfa0
yewyewXD <yewyew6933@gmail.com>
2023-12-26 08:36:22 +0700

    add hero texts

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,17 @@
 export default function Home() {
-  return <div>test</div>
+  return (
+    <div>
+      <section className="h-screen flex items-center">
+        <div className="container flex flex-col justify-center items-center text-center">
+          <h1 className="gradientText mb-6">{"Hi, I'm Kang Wei."}</h1>
+          <div className="text-lg font-medium mb-8">Bangkok, TH</div>
+          <p className="text-lg font-medium max-w-[500px]">
+            {
+              "I'm a React full-stack developer with 4 years of work experience."
+            }
+          </p>
+        </div>
+      </section>
+    </div>
+  )
 }
