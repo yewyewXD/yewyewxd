4a26ccc172b657164cb0db5752f4559e1919c071
yewyewXD <yewyew6933@gmail.com>
2023-12-27 09:31:12 +0700

    add nav open/close icons

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React, { useState } from 'react'
 
@@ -9,18 +10,35 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="fixed top-0 left-0 right-0">
+    <nav className="fixed top-0 left-0 right-0 z-50">
       <div className="container py-7 flex justify-end items-center">
         <div className="relative flex justify-end">
           <button
-            className="h-[60px] w-[60px] bg-white myShadow flex justify-center items-center rounded-full flex-shrink-0"
+            className="h-[60px] w-[60px] bg-white myShadow flex justify-center items-center rounded-full flex-shrink-0 relative"
             onClick={handleShowMenu}
           >
-            X
+            <Image
+              height={20}
+              width={20}
+              src="/images/icon_close.svg"
+              alt="Close button"
+              className={`absolute transition-opacity duration-300 ${
+                isMenuOpen ? 'opacity-100' : 'opacity-0'
+              }`}
+            />
+            <Image
+              height={20}
+              width={20}
+              src="/images/icon_menu.svg"
+              alt="Burger menu"
+              className={`absolute transition-opacity duration-300 ${
+                isMenuOpen ? 'opacity-0' : 'opacity-100'
+              }`}
+            />
           </button>
 
           <div
-            className={`bg-white navShadow top-[72px]  absolute p-2 rounded-xl w-64 text-lg transition-all duration-300 ${
+            className={`bg-white navShadow top-[72px] absolute p-2 rounded-xl w-64 text-lg transition-all duration-300 ${
               isMenuOpen
                 ? 'scale-100 opacity-100'
                 : 'scale-0 translate-x-24 -translate-y-6 opacity-0'
