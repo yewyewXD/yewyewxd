b3a45d353169ceaebed8a8b3e0089612f7f1f390
yewyewXD <yewyew6933@gmail.com>
2023-12-27 09:31:02 +0700

    remove highlight on hero texts

--- pages/components/PrimaryBtn.jsx
+++ pages/components/PrimaryBtn.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PrimaryBtn = ({ children }) => {
   return (
-    <button className="primaryButton py-4 px-12 rounded-full bg-primary text-lg font-bold z-50">
+    <button className="primaryButton py-4 px-12 rounded-full bg-primary text-lg font-semibold z-50 transition-transform duration-300 hover:scale-105 navShadow select-none">
       {children}
     </button>
   )
