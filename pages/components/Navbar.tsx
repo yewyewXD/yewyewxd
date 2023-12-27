7c78bfa246339fde09ef9c54dbf93f2bbc31582b
yewyewXD <yewyew6933@gmail.com>
2023-12-27 09:34:24 +0700

    disable scale on hover on mobile

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
       <div className="container py-7 flex justify-end items-center">
         <div className="relative flex justify-end">
           <button
-            className="h-[60px] w-[60px] bg-white myShadow flex justify-center items-center rounded-full flex-shrink-0 relative"
+            className="h-[60px] w-[60px] bg-white myShadow flex justify-center items-center rounded-full flex-shrink-0 relative transition-transform md:hover:scale-105"
             onClick={handleShowMenu}
           >
             <Image
@@ -22,7 +22,7 @@ const Navbar = () => {
               width={20}
               src="/images/icon_close.svg"
               alt="Close button"
-              className={`absolute transition-opacity duration-300 ${
+              className={`absolute transition-transform duration-300 ${
                 isMenuOpen ? 'opacity-100' : 'opacity-0'
               }`}
             />
