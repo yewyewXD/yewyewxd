b6c9e42d3c960bacf6eb756a8221877cbe9fd0e0
yewyewXD <yewyew6933@hotmail.com>
2025-02-14 17:42:43 +0800

    use google font weight

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,10 @@ import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
